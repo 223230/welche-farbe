a5c100a96020f146c363797400891ae8771f8456
223230 <sylveon_ari@hotmail.com>
2023-05-02 19:46:16 +0200

    Visuelles Update

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,8 @@ export default function Home() {
     let c = ColorNamer(color.hex, { pick: ['roygbiv', 'ntc'] });
     let simpleName = "Poah keine Ahnung";
     let hue = color.hsl.h;
+    
+
 
     if(color.hsl.s > .1) {
       if(hue < 19 || hue >= 350) { simpleName = "rot" }
@@ -78,122 +80,125 @@ export default function Home() {
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`
-        ${inter.className}
-        w-screen
-        min-h-screen
-        flex
-        justify-center
-        items-center
-        sm:p-4
-        bg-zinc-900
-        text-zinc-400
-        text-base
-        lg:text-lg
-      `}>
-        <div className='
-          sm:px-12
-          sm:py-8
-          lg:px-16
-          lg:py-12
-          lg:box-content
-          min-h-[20rem]
-          max-w-3xl
-          h-full
-          sm:h-fit
-          bg-zinc-800
-          sm:border
-          sm:rounded-2xl
-          sm:shadow-lg
-          lg:shadow-2xl
-          border-zinc-700
+      <div style={{backgroundColor: `rgb(${state.color.r}, ${state.color.g}, ${state.color.b})`}}>
+        <main className={`
+          ${inter.className}
+          w-screen
+          min-h-screen
           flex
-          flex-col
-          gap-y-6
-          lg:gap-y-8
-          grow
-        '>
-          <div className='space-y-2 px-6 pt-8 sm:p-0'>
-            <h1
-              className={`
-                text-5xl
-                lg:text-6xl
-                text-center
-                font-bold
-                ${interTight.className}
-                text-zinc-100
-                mb-1
-                lg:mb-2
-              `}
-            >
-              Welche Farbe
-            </h1>
-            <p className='text-center'>
-              Sagt in normalen Worten, wie eine Farbe heißt (100% Farbgarantie)
-            </p>
-            {colorName != "" && nerdName != "" ? (<>
-              <p className='text-center pt-8 text-xl lg:text-2xl font-bold'>
-                Aktuelle Farbe: {colorName}
-              </p>
+          justify-center
+          items-center
+          sm:p-4
+          bg-black/80
+          text-white/60
+          text-base
+        `}>
+          <div className='
+            sm:px-12
+            sm:py-8
+            lg:px-16
+            lg:py-12
+            lg:box-content
+            min-h-[20rem]
+            max-w-xl
+            h-full
+            sm:h-fit
+            bg-black/40
+            sm:border
+            sm:rounded-2xl
+            sm:shadow-lg
+            lg:shadow-2xl
+            border-white/20
+            flex
+            flex-col
+            gap-y-6
+            lg:gap-y-8
+            grow
+          '>
+            <div className='space-y-2 px-6 pt-8 sm:p-0'>
+              <h1
+                className={`
+                  text-5xl
+                  lg:text-6xl
+                  text-center
+                  font-bold
+                  ${interTight.className}
+                  text-white/80
+                  mb-1
+                  lg:mb-2
+                `}
+              >
+                Welche Farbe
+              </h1>
               <p className='text-center'>
-                &quot;{nerdName}&quot; 🤓
+                Sagt in normalen Worten, wie eine Farbe heißt (100% Farbgarantie)
               </p>
-            </>) : (<>
-              <p className='text-center pt-8 text-xl lg:text-2xl font-bold select-none opacity-0'>
-                .
-              </p>
-              <p className='text-center select-none opacity-0'>
-                .
-              </p>
-            </>)}
+              {colorName != "" && nerdName != "" ? (<div className='flex pt-8 gap-4 align-middle'>
+                <div
+                  className='w-16 aspect-square shrink-0 rounded-lg border border-white/30 shadow-lg'
+                  style={{backgroundColor: `rgb(${state.color.r}, ${state.color.g}, ${state.color.b})`}}
+                />
+                <div>
+                  <p className='text-xl lg:text-2xl font-bold text-white/80'>
+                    {colorName}
+                  </p>
+                  <p className=''>
+                    &quot;{nerdName}&quot; 🤓
+                  </p>
+                </div>
+              </div>) : (<>
+                <p className='text-center pt-8 text-xl lg:text-2xl font-bold select-none opacity-0'>
+                  .
+                </p>
+                <p className='text-center select-none opacity-0'>
+                  .
+                </p>
+              </>)}
+            </div>
+            <div className='mx-6 sm:mx-0'>
+              <ChromePicker color={ state.color } onChange={ handleChange }/>
+            </div>
+            <div className='grow'></div>
+            <div className='text-xs lg:text-sm p-6 sm:p-0 bg-black/20 sm:bg-transparent'>
+              <div className='text-white/30'>
+                Gemacht von {' '}
+                <Link
+                  href='https://twitter.com/_223230'
+                  className='text-white/60 hover:underline hover:text-white/80'
+                >
+                  Luna
+                </Link>,{' '}
+                Credit geht an {' '}
+                <Link
+                  href='https://twitter.com/HansGurkenbauer/'
+                  target='blank'
+                  className='text-white/60 hover:underline hover:text-white/80'
+                >
+                  Hans Gurkenbauer
+                </Link>.
+                Inspiriert durch {' '}
+                <Link
+                  href='https://twitter.com/HansGurkenbauer/status/1651726821761724419'
+                  target='blank'
+                  className='text-white/60 hover:underline hover:text-white/80'
+                >
+                  diesen Tweet
+                </Link>
+              </div>
+              <div className='text-white/30'>
+                Quellcode: {' '}
+                <Link
+                  href='https://github.com/223230/welche-farbe'
+                  target='blank'
+                  className='text-white/60 hover:underline hover:text-white/80'
+                >
+                  223230/welche-farbe
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className='mx-6 sm:mx-16'>
-            <ChromePicker color={ state.color } onChange={ handleChange }/>
-          </div>
-          <div className='grow'></div>
-          <div className='text-sm lg:text-base p-6 sm:p-0 bg-zinc-900 sm:bg-transparent'>
-            <div className='text-zinc-500'>
-              Gemacht von {' '}
-              <Link
-                href='https://twitter.com/_223230'
-                className='text-zinc-400 underline'
-              >
-                Luna
-              </Link>
-            </div>
-            <div className='text-zinc-500'>
-              Credit an {' '}
-              <Link
-                href='https://twitter.com/HansGurkenbauer/'
-                target='blank'
-                className='text-zinc-400 underline'
-              >
-                Hans Gurkenbauer
-              </Link>
-            </div>
-            <div className='text-zinc-500'>
-              Inspiriert durch {' '}
-              <Link
-                href='https://twitter.com/HansGurkenbauer/status/1651726821761724419'
-                target='blank'
-                className='text-zinc-400 underline'
-              >
-                diesen Tweet
-              </Link>
-            </div>
-            <div className='text-zinc-500'>
-              Quellcode: {' '}
-              <Link
-                href='https://github.com/223230/welche-farbe'
-                target='blank'
-                className='text-zinc-400 underline'
-              >
-                223230/welche-farbe
-              </Link>
-            </div>
-          </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </>
   )
 }
